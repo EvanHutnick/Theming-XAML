e63782e98475db18e06c84a7b65d760b4740e3ee
Evan Hutnick <evanhutnick@gmail.com>
2012-03-27 22:08:14 -0400

    adding class

---
+++ Inter.Theming.Test/Inter.Theming.Test/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inter.Theming.Test
+{
+    class TestClass
+    {
+        public int Id { get; set; }
+
+        public TestClass()
+        {
+
+        }
+    }
+}
